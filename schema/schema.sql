87aba656b1fbacfd74f9625887547bcb42bf92fa
Georg Brandt <georgbrandt-leipzig@gmx.de>
2026-05-31 23:30:09 +0200

    updated schema and .gitignore

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,13 +1,17 @@
 CREATE TABLE foods (
     	id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	name TEXT NOT NULL UNIQUE
+    	name TEXT NOT NULL UNIQUE,
+	source TEXT NOT NULL DEFAULT 'manual',
+	external_id TEXT
 );
 
 CREATE TABLE nutrients (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL UNIQUE,
 	unit TEXT NOT NULL
+);
 
+-- nutritional values per 100 g of food
 CREATE TABLE food_nutrients (
     	food_id INTEGER NOT NULL,
 	nutrient_id INTEGER NOT NULL,
@@ -19,19 +23,29 @@ CREATE TABLE food_nutrients (
 	FOREIGN KEY (nutrient_id) REFERENCES nutrients(id) ON DELETE CASCADE
 );
 
-CREATE TABLE recipe (
+CREATE TABLE recipes (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL UNIQUE,
 	description TEXT
 );
 
 CREATE TABLE recipe_items (
-    	recipe_id INTEGER NOT NULL,
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	recipe_id INTEGER NOT NULL,
 	food_id INTEGER NOT NULL,
 	amount_g REAL NOT NULL,
 
-	PRIMARY KEY (food_id, recipe_id)
-
 	FOREIGN KEY (recipe_id) REFERENCES recipe(id) ON DELETE CASCADE,
-	FOREIGN KEY (food_id) REFERENCES food(id)
+	FOREIGN KEY (food_id) REFERENCES foods(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE portion_sizes (
+	food_id INTEGER NOT NULL,
+	serving TEXT NOT NULL,
+	amount REAL NOT NULL,
+	unit TEXT NOT NULL CHECK (unit IN('g', 'ml', 'piece')),
+
+	PRIMARY KEY (food_id, serving)
+
+	FOREIGN KEY (food_id) REFERENCES foods(id)
 );
